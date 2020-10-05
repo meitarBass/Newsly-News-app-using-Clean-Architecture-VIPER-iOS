747a3fbb725ec21ab8752a1a8d4a827848ddd6d4
Ebubechukwu <dimobiblaise@gmail.com>
2020-10-05 18:51:04 +0300

    Initial Commit

---
+++ Newsly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Newsly
+//
+//  Created by Ebubechukwu Dimobi on 05.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
